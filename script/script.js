9c178f7cf3452023c5357ed787c4d4ffae7d30de
Jonathan Parras <jparras@email.com>
2023-03-28 16:59:57 -0700

    adds Light mode button

--- script/script.js
+++ script/script.js
@@ -1,16 +1,30 @@
 let darkthemeButton = document.querySelector(".dark-theme");
 
-function darkMode () {
-    const mySidebar = document.querySelector("body > nav");
-    const textArea = document.querySelector("textarea")
+function darkMode (event) {
+    var mySidebar = document.querySelector("body > nav");
+    var textArea = document.querySelector("textarea");
 
-    mySidebar.classList.remove("nav-light-mode");
-    mySidebar.classList.add("nav-dark-mode");
-    textArea.classList.remove("nav-light-mode");
-    textArea.classList.add("nav-dark-mode");
+    mySidebar.classList.toggle("nav-light-mode");
+    mySidebar.classList.toggle("nav-dark-mode");
+    textArea.classList.toggle("nav-light-mode");
+    textArea.classList.toggle("nav-dark-mode");
     
-    document.body.classList.remove("light-mode");
-    document.body.classList.add("dark-mode");
+    document.body.classList.toggle("light-mode");
+    document.body.classList.toggle("dark-mode");
+
+    if (event.target.tagName === "BUTTON" && darkthemeButton.textContent === "Dark Theme") {
+        darkthemeButton.textContent = "Light Theme";
+    } else {
+        darkthemeButton.textContent = "Dark Theme";
+    }
 }
 
 darkthemeButton.addEventListener("click", darkMode);
+
+let cancelButton = document.querySelector(".cancel");
+
+function hideTextArea (event) {
+    
+}
+
+cancelButton.addEventListener("click", hideTextArea)
